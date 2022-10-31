cf889bada112732f591f607e5309c8baeee497bb
mateusgomes <mateus.gomes.clear@gmail.com>
2022-10-31 15:41:08 -0300

    Component Button #02

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <div>
-
-    <React.StrictMode>
+      <h1>Calculator</h1>
       <Calculator />
-    </React.StrictMode>
-
   </div>
 );
 
